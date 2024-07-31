947c0c7eae6b7f03ab494ff0f38b2d789756dee1
高平齐 Gao Pingqi <42695156+MrPingQi@users.noreply.github.com>
2024-07-31 17:39:04 +0800

    Add files via upload

---
+++ Multi-scale PIIFD for image registration/Get_Multiscale_PIIFD.m
@@ -0,0 +1,44 @@
+function descriptors = Get_Multiscale_PIIFD(I,kps,...
+    nOctaves,nLayers,G_resize,G_sigma,scale,NBS,NBO,int_flag,rot_flag)
+
+sig = Get_Gaussian_Scale(G_sigma,nLayers);
+descriptors = cell(nOctaves,nLayers); I_t = [];
+for octave=1:nOctaves
+    for layer=1:nLayers
+        if octave>1
+            kps_t = round(kps(:,1:2)./G_resize^(octave-1));
+            [~,index,~] = unique(kps_t,'rows');
+        else
+            kps_t = kps(:,1:2);
+            index = 1:size(kps,1);
+        end
+        I_t = Gaussian_Scaling(I,I_t,octave,layer,G_resize,sig(layer));
+        descriptor = PIIFD_Descriptor(I_t, kps(index,1:2), kps_t(index,:), ...
+            scale, NBS, NBO, int_flag, rot_flag);
+        descriptors{octave,layer} = descriptor(:,[3,4,1,2,5:end]);
+    end
+end
+
+
+function sig = Get_Gaussian_Scale(sigma,numLayer)
+sig = zeros(1,numLayer);
+sig(1) = sigma;
+k = 2^(1.0/(numLayer-1));
+for i = 2:numLayer
+    sig_previous = k^(i-2)*sigma;
+    sig_current = k*sig_previous;
+    sig(i) = sqrt(sig_current^2-sig_previous^2);
+end
+
+
+function I_t = Gaussian_Scaling(I,I_t,Octave,Layer,G_resize,sig)
+if(Octave==1 && Layer==1)
+    I_t = I;
+elseif(Layer==1)
+    I_t = imresize(I,1/G_resize^(Octave-1),'bilinear');
+else
+    W = round(2*sig);
+    W = 2*W+1;
+    w = fspecial('gaussian',[W,W],sig);
+    I_t = imfilter(I_t,w,'replicate');
+end
